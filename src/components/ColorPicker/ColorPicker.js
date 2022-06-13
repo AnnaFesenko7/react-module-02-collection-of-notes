0a1a23b2d53a66a8e6428038b3262611d7046e76
AnnaFesenko7 <annafecenko7@gmail.com>
2022-06-13 16:27:57 +0200

    colorpicker by hook

--- src/components/ColorPicker/ColorPicker.js
+++ src/components/ColorPicker/ColorPicker.js
@@ -1,52 +1,72 @@
 import './ColorPicker.scss';
 import classNames from 'classnames';
 
-import React, { Component } from 'react';
+import { useState } from 'react';
+// import React, { Component } from 'react';
 
-class ColorPicker extends Component {
-  state = {
-    activeOptionIndex: 0,
-  };
-  setActiveIndex = index => {
-    this.setState({ activeOptionIndex: index });
-  };
-  // makeOptionClassName = (index, activeOptionIndex) => {
-  //   return classNames('ColorPickerOption', {
-  //     ColorPickerActive: index === activeOptionIndex,
-  //   });
-  // };
-  render() {
-    const { options } = this.props;
-    const { activeOptionIndex } = this.state;
-    const { label } = options[this.state.activeOptionIndex];
-    return (
-      <div className="ColorPicker">
-        <h1 className="ColorPickerTitle">ColorPicker</h1>
-        <p>Выбран цвет:{label}</p>
-        <div>
-          {options.map((option, index) => {
-            return (
-              <span
-                key={option.color}
-                style={{ backgroundColor: option.color }}
-                className={
-                  classNames('ColorPickerOption', {
-                    ColorPickerActive: index === activeOptionIndex,
-                  })
-                  // index === activeOptionIndex
-                  //   ? 'ColorPickerOption ColorPickerActive'
-                  //   : 'ColorPickerOption'
-                }
-                onClick={() => {
-                  this.setActiveIndex(index);
-                }}
-              ></span>
-            );
-          })}
-          ;
-        </div>
+function ColorPicker(options) {
+  const [activeOptionIndex, setActiveOptionIndex] = useState(0);
+
+  const { label } = options.options[activeOptionIndex];
+
+  return (
+    <div className="ColorPicker">
+      <h1 className="ColorPickerTitle ">ColorPicker by hook</h1>
+      <p>Выбран цвет:{label}</p>
+      <div>
+        {options.options.map((option, index) => {
+          return (
+            <span
+              key={option.color}
+              style={{ backgroundColor: option.color }}
+              className={classNames('ColorPickerOption', {
+                ColorPickerActive: index === activeOptionIndex,
+              })}
+              onClick={() => {
+                setActiveOptionIndex(index);
+              }}
+            ></span>
+          );
+        })}
       </div>
-    );
-  }
+    </div>
+  );
 }
+// class ColorPicker extends Component {
+//   state = {
+//     activeOptionIndex: 0,
+//   };
+//   setActiveIndex = index => {
+//     this.setState({ activeOptionIndex: index });
+//   };
+
+//   render() {
+//     const { options } = this.props;
+//     const { activeOptionIndex } = this.state;
+//     const { label } = options[this.state.activeOptionIndex];
+//     return (
+//       <div className="ColorPicker">
+//         <h1 className="ColorPickerTitle by hook">ColorPicker</h1>
+//         <p>Выбран цвет:{label}</p>
+//         <div>
+//           {options.map((option, index) => {
+//             return (
+//               <span
+//                 key={option.color}
+//                 style={{ backgroundColor: option.color }}
+//                 className={classNames('ColorPickerOption', {
+//                   ColorPickerActive: index === activeOptionIndex,
+//                 })}
+//                 onClick={() => {
+//                   this.setActiveIndex(index);
+//                 }}
+//               ></span>
+//             );
+//           })}
+//           ;
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 export default ColorPicker;
